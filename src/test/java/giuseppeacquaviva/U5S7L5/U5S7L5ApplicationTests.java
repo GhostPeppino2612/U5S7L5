ac04fce8284dc7bf91db309d3fb323c91ff20fb5
GhostPeppino2612 <giuseacqua6@gmail.com>
2024-10-16 10:31:38 +0200

    completo esercizio

---
+++ src/test/java/giuseppeacquaviva/U5S7L5/U5S7L5ApplicationTests.java
@@ -0,0 +1,13 @@
+package giuseppeacquaviva.U5S7L5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5S7L5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
